3aba603ddabf9ef2efc7b4015125e880877581af
Francisco Marin <francisco.marin@ceiba.com.co>
2020-02-25 07:43:51 -0500

    cambios en controller
    -se activa la configuracion de CORS

--- cafe-internet-api/src/main/java/com/ceiba/cafe/adapter/web/CategoryController.java
+++ cafe-internet-api/src/main/java/com/ceiba/cafe/adapter/web/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import com.ceiba.cafe.domain.Category;
 
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @WebAdapter
 @RestController
 @RequestMapping("cafe-api/category")
